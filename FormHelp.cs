b702f7490c8633d8f2a83c5520fdd455d416ffcb
Sergey Lupov <sergey.lupov@pselab.ru>
2014-07-13 18:05:56 +0400

    unused referenses are removed

--- FormHelp.cs
+++ FormHelp.cs
@@ -23,10 +23,10 @@
  ***************************************************************************/
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
 //using System.Linq;
 using System.Text;
 using System.Windows.Forms;
--- e-talker.cs
+++ e-talker.cs
@@ -23,12 +23,12 @@
  ***************************************************************************/
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
 using System.Drawing;
 //using System.Linq;
-using System.Text;
+//using System.Text;
 //using System.Threading.Tasks;
 using System.Windows.Forms;
 
